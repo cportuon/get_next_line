168ea979b3a0a7be3036711e1ee96e365de24f4e
Cosme Portuondo Barcena <cportuon@c3r5s2.42urduliz.com>
2022-11-21 17:57:16 +0100

    Imprime la primera línea

--- get_next_line.c
+++ get_next_line.c
@@ -12,11 +12,39 @@
 
 #include "get_next_line.h"
 
+char    *get_next_line(int fd)
+{   
+    int     i;
+    char    *buff;
+    size_t  read_line;
+
+    i = 0;
+    buff = malloc(sizeof(char) * BUFFER_SIZE);
+    read_line = read(fd, buff, BUFFER_SIZE);
+    if (fd <= 0)
+        return (NULL);
+    if (!buff)
+        return (NULL);
+    while(buff[i])
+    {
+        if (buff[i] == '\n')
+            return (0);
+        write(1, &buff[i], 1);
+        i++;
+    }
+    return (0);
+}
 
 int main(void)
 {
-    char    *str;
+    int     fd;
+    char    *buff;
 
-    str = "";
+    fd = open("read.txt", O_RDONLY);
+    buff = malloc(sizeof(char) * BUFFER_SIZE);
+    //printf("%zd", read(fd, buff, BUFFER_SIZE));
+    //printf("%s", buff);
+    get_next_line(fd);
+    close(fd);
     return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -13,8 +13,13 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include<stdio.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
 
-
+char *get_next_line(int fd);
 
 #endif
