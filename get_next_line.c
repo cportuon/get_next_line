b72cc88ba366ae123059c38f9ebaee247f91db16
Cosme Portuondo Barcena <cportuon@c3r2s5.42urduliz.com>
2022-12-19 14:25:52 +0100

    Imprime una linea y la guarda

--- get_next_line.c
+++ get_next_line.c
@@ -12,49 +12,49 @@
 
 #include "get_next_line.h"
 
-char    *get_next_line(int fd);
+char    *get_next_line(int fd)
+{
+    int     i;
+    int     j;
+    char    *buff;
+    size_t  read_line;
+    char    *line;
+    
+    i = 0;
+    j = 0;
+    buff = malloc(sizeof(char) * BUFFER_SIZE + 100);
+    line = malloc(sizeof(char) * BUFFER_SIZE + 100);
+    read_line = read(fd, buff, BUFFER_SIZE);
+    buff[read_line] = '\0';
+    if (fd <= 0 || BUFFER_SIZE == 0)
+        return (NULL);
+    if (read_line == 0)
+        return(NULL);
+    while(buff[i] && buff[i] != '\n')
+    {
+        line[j] = buff[i];
+        j++;
+        i++;
+        if (!buff[i])
+        {
+            i = 0;
+            read_line = read(fd, buff, BUFFER_SIZE);
+            buff[read_line] = '\0';
+        }
+    }
+    line[j] = '\0'; 
+    return (line);
+}
 
 int main(void)
 {
+    char    *lectura;
     int     fd;
-    char    *buff;
 
     fd = open("read.txt", O_RDONLY);
-    printf("main_fd: %i\n", fd);//----------------variable fd
-    buff = malloc(sizeof(char) * BUFFER_SIZE);
-    printf("main_buff: %s\n", buff);//----------------variable buff
-    get_next_line(fd);
-    close(fd);
-    return (0);
-}
-
-char    *get_next_line(int fd)
-{
-    printf("gnl_fd: %i\n", fd);//----------------variable  fd
-    int     i;
-    char    *buff;
-    size_t  read_line;
-    
-    i = 0;
-    buff = malloc(sizeof(char) * BUFFER_SIZE);
-    printf("gnl_buff_1: %s\n", buff);//----------------variable buff
-    read_line = read(fd, buff, BUFFER_SIZE);
-    printf("gnl_read_line: %zu\n", read_line);//----------------variable read_line
-    printf("gnl_BUFFER_SIZE: %d\n", BUFFER_SIZE);//----------------variable BUFFER_SIZE
-    if (fd <= 0)
-        return (NULL);
-    if (!buff)
-        return (NULL);
-    while(buff[i] && buff[i] != '\n')
-    {
-        if (buff[i] == '\n')
-        {
-            write(1, "\n", 1);
-            printf("gnl_buff_2: %s\n", buff);//----------------variable buff
-            return (0);
-        }
-        write(1, &buff[i], 1);
-        i++;
-    }
+    lectura = get_next_line(fd);
+    lectura = get_next_line(fd);
+    printf("%s", lectura);
+    close (fd);
     return (0);
 }
