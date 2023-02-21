4d186731457cb87d4bfa08e36028135cab29cbb4
Cosme Portuondo Barcena <cportuon@c4r1s3.42urduliz.com>
2023-02-21 20:20:08 +0100

    normi OK, Falta revision PACO

--- get_next_line.h
+++ get_next_line.h
@@ -17,17 +17,13 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-char    *get_next_line(int fd);
-char    *ft_strjoin(char *aux, char *buff);
-int     ft_strlen(char *str);
-char    *ft_strdup(char *s);
-char    *ft_substr(char *line, int	start, int strlen2);
-char    *ft_strchr(const char *str, int c);
-char    *ft_read_line(int fd, char *backup, char *buff);
-char    *ft_get_rest(char *line);
-
-# ifndef BUFFER_SIZE
-#   define BUFFER_SIZE 100
-# endif
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *aux, char *buff);
+int		ft_strlen(char *str);
+char	*ft_strdup(char *s);
+char	*ft_substr(char *line, int start, int strlen2);
+char	*ft_strchr(const char *str, int c);
+char	*ft_read_line(int fd, char *backup, char *buff);
+char	*ft_get_rest(char *line);
 
 #endif
--- main.c
+++ main.c
@@ -1,19 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cportuon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 20:02:31 by cportuon          #+#    #+#             */
+/*   Updated: 2023/02/21 20:02:34 by cportuon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 int	main(void)
 {
-	int	fd;
+	int		fd;
 	char	*read;
 
 	fd = open("read.txt", O_RDONLY);
-	read = 	get_next_line(fd);
-	printf("First line: %s\n", read);
+	read = get_next_line (fd);
+	printf ("First line: %s\n", read);
 	free(read);
-	read = 	get_next_line(fd);
-	printf("Second line: %s\n", read);
+	read = get_next_line (fd);
+	printf ("Second line: %s\n", read);
 	free(read);
-	read = 	get_next_line(fd);
-	printf("Third line: %s\n", read);	
+	read = get_next_line (fd);
+	printf ("Third line: %s\n", read);
 	free(read);
-	return(0);
+	return (0);
 }
